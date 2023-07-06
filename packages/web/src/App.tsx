fcbfb4b16c4e5d43fa29fffbcb7c76424b36bab0
Jennifer Yen <jennifer.yenjy@gmail.com>
2023-07-06 12:52:16 -0700

    fix collision

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -100,8 +100,6 @@ export const App = () => {
 
               {/* Spawns coins, spikes, platforms */}
               <Entities />
-
-              {/* <SpikedPlatform /> */}
             </Physics>
           </>
         )}
--- packages/web/src/SpikedPlatform.tsx
+++ packages/web/src/SpikedPlatform.tsx
@@ -12,8 +12,8 @@ interface SpikedPlatformProps extends RigidBodyProps {
 }
 
 export const SpikedPlatform = ({
-  position = new Vector3(),
-  platformSize = new Vector3(2, 1, 0),
+  position = new Vector3(3, 2, 0),
+  platformSize = new Vector3(1, 1, 0),
   spikeSize = new Vector3(1, 1, 0),
   orientation = "top",
   ...props
@@ -31,20 +31,15 @@ export const SpikedPlatform = ({
       spikePosition.y += platformSize.y / 2 + spikeSize.y - 0.5;
       break;
     case "bottom":
-      spikePosition.y -= platformSize.y / 2 + spikeSize.y;
+      spikePosition.y -= platformSize.y / 2 + spikeSize.y - 0.5;
       break;
     case "left":
-      spikePosition.x -= platformSize.x / 2 + spikeSize.x;
+      spikePosition.x -= platformSize.x / 2 + spikeSize.x - 0.5;
       break;
     case "right":
-      spikePosition.x += platformSize.x / 2 + spikeSize.x;
-      break;
-    default:
-      spikePosition.y += platformSize.y / 2 + spikeSize.y;
+      spikePosition.x += platformSize.x / 2 + spikeSize.x - 0.5;
       break;
   }
-
-  console.log("Spike position", spikePosition);
 
   return (
     <group position={position}>
@@ -59,7 +54,6 @@ export const SpikedPlatform = ({
         position={spikePosition}
         onCollisionEnter={({ other }) => {
           if (other.rigidBodyObject?.name !== "player") return;
-          console.log("Colliding"); // Why is this not logging :(
           store?.player.ref?.current?.setTranslation(
             new Vector3(0, 2, 0),
             false
@@ -68,7 +62,10 @@ export const SpikedPlatform = ({
         }}
         {...props}
       >
-        <primitive object={spikeSprite} />
+        <mesh>
+          <boxGeometry args={spikeSize.toArray()} />
+          <primitive object={spikeSprite} />
+        </mesh>
       </RigidBody>
     </group>
   );
