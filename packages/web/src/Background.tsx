1c80056403719d4dcc58607715930989db594864
catoenm <mitch@phantom.app>
2023-07-06 15:52:02 -0700

    Fixing background

--- packages/web/src/Background.tsx
+++ packages/web/src/Background.tsx
@@ -3,13 +3,13 @@ import * as THREE from "three";
 import React from "react";
 
 export const Background = () => {
-  const texture = useLoader(THREE.TextureLoader, "src/sprites/background.png");
+  const texture = useLoader(THREE.TextureLoader, "/sprites/background.png");
   texture.wrapS = texture.wrapT = THREE.RepeatWrapping;
   texture.repeat.set(1, 3); // Adjust the repeat values as needed
 
   return (
     <mesh position={[0, 20, 0]}>
-      <primitive object={new THREE.PlaneGeometry(18, 100)} attach="geometry" />
+      <primitive object={new THREE.PlaneGeometry(16, 100)} attach="geometry" />
       <primitive
         object={new THREE.MeshBasicMaterial({ map: texture })}
         attach="material"
