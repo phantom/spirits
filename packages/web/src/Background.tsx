e7c0ea4104540411a1214d4848eb59ecec50e85e
catoenm <mitch@phantom.app>
2023-07-06 15:48:18 -0700

    Stretching background

--- packages/web/src/Background.tsx
+++ packages/web/src/Background.tsx
@@ -6,7 +6,7 @@ export const Background = () => {
   const texture = useLoader(THREE.TextureLoader, "src/sprites/background.png");
 
   return (
-    <mesh position={[0, 47, 0]}>
+    <mesh position={[0, 20, 0]}>
       <primitive object={new THREE.PlaneGeometry(18, 100)} attach="geometry" />
       <primitive
         object={new THREE.MeshBasicMaterial({ map: texture })}
