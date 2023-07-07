23c38b9d60885801f15b075d56e511dc7caea814
Dries Croons <dries@phantom.app>
2023-07-07 10:40:14 +0200

    level loading

--- packages/web/src/store.ts
+++ packages/web/src/store.ts
@@ -174,11 +174,18 @@ useStore.setState((store) => {
   let height = 0;
 
   const entities = [tutorialLevel, testRoom, endLevel].reduce((agg, init) => {
-    const entities = init(height);
-    height += entities.sort((a, b) => b.position[1] - a.position[1])[0]
-      .position[1];
+    const newEntities = init.map((e) => {
+      return {
+        ...e,
+        position: [e.position[0], e.position[1] + height, e.position[2]],
+      };
+    });
+    const additionalHeight = init.sort(
+      (a, b) => b.position[1] - a.position[1]
+    )[0].position[1];
+    height += additionalHeight;
 
-    return [...agg, ...(entities as any)];
+    return [...agg, ...(newEntities as any)];
   }, []) as unknown as any[];
 
   // side, top and bottom platforms get automatically added
